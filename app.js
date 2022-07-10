1fbaa934d8226f88488e4e238a154f75a24ebdaa
Rakesh Gain <rakeshgain47681@gmail.com>
2022-07-10 07:30:49 +0530

    post route middleware added

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
+require("dotenv").config();
 const express = require('express');
+const bodyParser = require('body-parser');
 const connectDB = require('./config/database');
 
 // Import Routes
 const authRoute = require('./routes/auth');
+const postRoute = require('./routes/posts');
 
 const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 //Route Middlewares
 app.use('/api/user',authRoute)
+app.use('/api/posts',postRoute)
 
 // Connect to MongoDB
 connectDB();
